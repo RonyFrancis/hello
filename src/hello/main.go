b549dd3ed5e56c4872413be5562788e4270d2b9d
Antony Rony <antony.rony@mobme.in>
2017-01-14 20:59:58 +0530

    post call

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 func main() {
-	var msg string
-	msg = "hello world using variable"
-	fmt.Printf(msg)
+	v := url.Values{}
+	v.Set("msisdn", "8J0sE0LtvIAxRfubIDBiGg==") // encrypted msisdn 1212121212
+	//custid := "vPTGEkzVsvLGOcF77PVa9g=="
+	//phoneNo := "8J0sE0LtvIAxRfubIDBiGg=="
+
+	resp, err := http.PostForm("https://send.bobsecure.com/v3/users/register", v)
+	// post call
+	if err != nil {
+		os.Exit(0)
+	}
+
+	robots, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		os.Exit(0)
+	}
+	//resp.Body.close()
+
+	fmt.Printf(string(robots))
+
 }
