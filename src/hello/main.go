6d7ee7d8be78c2e899f9e73e8c1213c0edaa0ab2
Antony Rony <antony.rony@mobme.in>
2017-02-10 18:57:20 +0530

    loop_callas

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
+	t1 := time.Now()
+	var c chan string = make(chan string)
+	for i := 1; i <= 10; i++ {
+
+		fmt.Print("#")
+		fmt.Println(postcall())
+	}
+	t2 := time.Now()
+	fmt.Println(t1.Sub(t2))
+
+}
+
+func postcall() string {
 	v := url.Values{}
-	v.Set("msisdn", "8J0sE0LtvIAxRfubIDBiGg==") // encrypted msisdn 1212121212
+	v.Set("no", strconv.Itoa(rand.Intn(100))) // encrypted msisdn 1212121212
 	//custid := "vPTGEkzVsvLGOcF77PVa9g=="
 	//phoneNo := "8J0sE0LtvIAxRfubIDBiGg=="
 
-	resp, err := http.PostForm("https://send.bobsecure.com/v3/users/register", v)
+	resp, err := http.PostForm("http://localhost:3000/uk/welcome", v)
 	// post call
 	if err != nil {
 		os.Exit(0)
@@ -26,6 +42,6 @@ func main() {
 	}
 	//resp.Body.close()
 
-	fmt.Printf(string(robots))
+	return string(robots)
 
 }
